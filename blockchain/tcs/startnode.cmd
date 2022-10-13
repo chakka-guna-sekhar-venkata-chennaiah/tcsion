04e42848fbd7f153fe7a22cbc0e6c29cfba515fe
software-babooi <110555361+software-babooi@users.noreply.github.com>
2022-10-13 23:11:35 +0530

    Add files via upload

---
+++ blockchain/tcs/startnode.cmd
@@ -0,0 +1,1 @@
+geth --networkid 43352 --mine --miner.threads 2 --datadir "." --nodiscover --http --allow-insecure-unlock --http.port "8545" --port "30303" --http.corsdomain "*" --nat "any" --http.api eth.web3,personal,net --unlock 0 --password ./password.sec
